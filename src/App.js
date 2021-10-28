ba92886144c95174cf2003d9701f4997a1976348
Andre Grant <andrethegrant@gmail.com>
2021-10-28 13:16:49 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
-import { useState } from 'react'
+import "./App.css";
+import { useState } from "react";
+import {Header} from "./components/header"
 
-import Map from './components/Map'
+import Map from "./components/Map";
 
 function App() {
-
-  const [center, setCenter] = useState([43.88, -72.7317])
+  const [center, setCenter] = useState([43.88, -72.7317]);
 
   return (
     <div>
       <Map center={center} />
     </div>
+    
+    <Header/>
+    <Info/>
   );
+<Header/>
 }
 
 export default App;
