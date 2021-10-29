0abb049e883dda8be3e4aa63879cb0f369fd0c9e
Trentp26 <trentpoitras@gmail.com>
2021-10-29 15:28:57 -0400

    addded a center point on the click of start. that centers tot he new location

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import { useState } from "react";
 import Map from "./components/Map";
+import borderData from "./data/border";
+import L from "leaflet";
+import leafletPip from "leaflet-pip";
 
 function App() {
   //use states
@@ -15,20 +18,37 @@ function App() {
   // All buttons besides start are disabled. On click start is disabled, other buttons are enabled.
 
   //max and min longitude and latitude of vermont
-  let maxLat = 45.005419;
-  let minLat = 42.730315;
-  let maxLong = -71.510225;
-  let minLong = -73.35218;
-  // taking max in min to get random longitude and latitude inside vermont
-  let latRange = maxLat - minLat + 1;
-  let longRange = maxLong - minLong + 1;
-  //setting the lat and long to random inside vermont
-  let newLat = Math.random() * latRange + minLat;
-  let newLong = Math.random() * longRange + minLong;
+  function randomCords() {
+    let poly = L.geoJson(borderData);
+    let maxLat = 45.005419;
+    let minLat = 42.730315;
+    let maxLong = -71.510225;
+    let minLong = -73.35218;
+    // taking max in min to get random longitude and latitude inside vermont
+    let latRange = maxLat - minLat + 1;
+    let longRange = maxLong - minLong + 1;
+    //setting the lat and long to random inside vermont
+    let newLat = Math.random() * latRange + minLat;
+    let newLong = Math.random() * longRange + minLong;
+    console.log(newLat);
+    console.log(newLong);
 
-  console.log(newLat);
-  console.log(newLong);
+    let results = leafletPip.pointInLayer([newLong, newLat], poly);
+    console.log(results);
 
+    return { newLat, newLong, results };
+  }
+
+
+  
+  let randoFunc = randomCords();
+while(randoFunc.results.length === 0) {
+    randoFunc = randomCords();
+  }
+
+  console.log(randomCords());
+
+  console.log(randoFunc.newLat);
   return (
     //wrapping flex box
     <div id="wrapper">
@@ -40,7 +60,7 @@ function App() {
           disabled={disable}
           onClick={() => {
             setDisable(true);
-            setCenter([newLat, newLong]);
+            setCenter([randoFunc.newLat, randoFunc.newLong]);
           }}
         >
           START
--- src/components/Map.js
+++ src/components/Map.js
@@ -14,30 +14,36 @@ import { layerGroup , L } from "leaflet";
 
 function Map(props) {
 const [point, setPoint] = useState([props.lat, props.long])
+const [zoom, setZoom] = useState(8)
 
   let vtOutline = borderData.geometry.coordinates[0].map((coords) => [
     coords[1],
     coords[0],
   ]);
 
+  
+function zoomInOut(props){
+  let zoom = MapContainer.zoom
+  return(
+  console.log(zoom))
+}
+zoomInOut()
 
 
-  useEffect(() => {
+
+
+  // useEffect(() => {
     
    
-    //turn border data into a L.geoJson
-    //import random Lat and long data here 
+  //   //turn border data into a L.geoJson
+  //   //import random Lat and long data here 
     
 
-    //then set up leaflet pip to a vareble 
-    // set up and if else lodgic to check result 
-    //leaflet pip will return an array if the point is in the geo json the array will have the geo json in it. and the array will be empty if it is not in vermont 
+  //   //then set up leaflet pip to a vareble 
+  //   // set up and if else lodgic to check result 
+  //   //leaflet pip will return an array if the point is in the geo json the array will have the geo json in it. and the array will be empty if it is not in vermont 
 
-    // $.getJSON("./data/border.js",function(data){
-    //   // L.geoJson function is used to parse geojson file and load on to map
-    //   L.geoJson(data).addTo(newMap);
-    //   });
-  }, [])
+  // }, [])
 
 
 
